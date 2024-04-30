7666acbf9c4abaec817dd6161a4ab1e82ad64de5
natesturdevant <70171610+natesturdevant@users.noreply.github.com>
2024-04-30 12:53:51 -0400

    Add project files.

---
+++ RootScreen.cs
@@ -0,0 +1,43 @@
+﻿using SadConsole.Input;
+
+namespace SadConsoleGame;
+
+internal class RootScreen : ScreenObject
+{
+    private Map _map;
+
+    public RootScreen()
+    {
+        _map = new Map(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
+        Children.Add(_map.SurfaceObject);
+    }
+
+    public override bool ProcessKeyboard(Keyboard keyboard)
+    {
+        bool handled = false;
+
+        if (keyboard.IsKeyPressed(Keys.Up))
+        {
+            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Up, _map);
+            handled = true;
+        }
+        else if (keyboard.IsKeyPressed(Keys.Down))
+        {
+            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Down, _map);
+            handled = true;
+        }
+
+        if (keyboard.IsKeyPressed(Keys.Left))
+        {
+            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Left, _map);
+            handled = true;
+        }
+        else if (keyboard.IsKeyPressed(Keys.Right))
+        {
+            _map.UserControlledObject.Move(_map.UserControlledObject.Position + Direction.Right, _map);
+            handled = true;
+        }
+
+        return handled;
+    }
+}
